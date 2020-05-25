932c36b414ee8aa8b0aa1d04703d5e74bca13e33
Jurian Vink <jurianvnk@gmail.com>
2020-05-25 12:45:19 +0200

    meter improved

--- components/meter/index.tsx
+++ components/meter/index.tsx
@@ -6,18 +6,44 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 const Body = styled.div`
-  padding: 12px 24px 84px 24px;
-
-  @media (min-width: 1100px) {
-    padding-bottom: 64px;
-  }
-
-  @media (min-width: 1300px) {
-    padding-bottom: 48px;
-  }
+  padding: 12px 24px 48px 24px;
 `;
 const Label = styled.h3`
   color: ${props => props.theme.fontColor};
+`;
+const Bar = styled.div`
+  position: relative;
+  display: flex;
+  height: 20px;
+  width: 100%;
+  border-radius: 8px;
+`;
+const MinBedCap = styled.div`
+  margin-top: 20px;
+  position: absolute;
+  color: ${props => props.theme.hintColor};
+`;
+const MaxBedCap = styled.div`
+  color: ${props => props.theme.hintColor};
+  right: 0;
+  top: 20px;
+  position: absolute;
+`;
+const Green = styled.div`
+  background-color: green;
+  width: 50%;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+`;
+const Orange = styled.div`
+  background-color: orange;
+  width: 30%;
+`;
+const Red = styled.div`
+  background-color: red;
+  width: 20%;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
 `;
 
 interface MeterProps {
@@ -30,20 +56,41 @@ const Meter = (props: MeterProps) => {
   const array = Array.from(Array(Math.round(max / 100)).keys());
   const getOffset = (value: number): string => `${Math.round(value / array.length)}%`;
 
+  const Pointer = styled.div`
+    position: absolute;
+    left: ${getOffset(value)};
+    top: -5px;
+    width: 3px;
+    height: 30px;
+    background-color: ${props => props.theme.fontColor};
+  `;
+  const BedsUsed = styled.div`
+    color: ${props => props.theme.hintColor};
+    position: absolute;
+    left: ${getOffset(value)};
+    top: 20px;
+  `;
+
   return (
     <Wrapper>
       <Card>
         <Body>
           <Label>IC-capaciteit</Label>
-          <div style={{ position: 'relative', display: 'flex', height: '20px', width: '100%', backgroundColor: 'blue', borderRadius: '8px' }}>
-            <div style={{ marginTop: '20px', position: 'absolute' }}><p>0</p></div>
-            <div style={{ backgroundColor: 'green', width: '50%', borderTopLeftRadius: '8px', borderBottomLeftRadius: '8px' }} />
-            <div style={{ backgroundColor: '#000', marginLeft: getOffset(value), width: '3px', height: '40px', marginTop: '-10px', position: 'absolute' }} />
-            <div style={{ marginLeft: getOffset(value), marginTop: '20px', position: 'absolute' }}><p>{value} bedden in gebruik</p></div>
-            <div style={{ backgroundColor: 'orange', width: '30%' }} />
-            <div style={{ backgroundColor: 'red', width: '20%', borderTopRightRadius: '8px', borderBottomRightRadius: '8px' }} />
-            <div style={{ marginLeft: '80%', marginTop: '20px', position: 'absolute' }}><p>{max} bedden beschikbaar</p></div>
-          </div>
+          <Bar>
+            <Green />
+            <Orange />
+            <Red />
+            <Pointer />
+            <MinBedCap>
+              <p>0</p>
+            </MinBedCap>
+            <BedsUsed>
+              <p>{value} bedden in gebruik</p>
+            </BedsUsed>
+            <MaxBedCap>
+              <p>{max} bedden beschikbaar</p>
+            </MaxBedCap>
+          </Bar>
         </Body>
       </Card>
     </Wrapper>
