3b0102ba8d707e827f672adf9992bf839ab5896b
Jurian Vink <jurianvnk@gmail.com>
2020-05-26 00:21:12 +0200

    fixed mobile

--- components/graph-item/index.tsx
+++ components/graph-item/index.tsx
@@ -15,11 +15,22 @@ const Wrapper = styled.div`
 const Body = styled.div`
   padding: 1.5rem;
 `;
+const HeaderDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
 const SwitchDiv = styled.div`
   display: flex;
   align-items: center;
 `;
-const SwitchText = styled.h4`
+const SwitchText = styled.span`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
   color: ${props => props.theme.fontColor};
   margin-right: 1rem;
 `;
@@ -38,6 +49,7 @@ const GraphItem = (props: GraphItemProps) => {
   const { value } = useDarkMode(false);
   const [isCumulative, setCumulative] = useState(false);
   const toggle = () => setCumulative(!isCumulative);
+  const dataColor = value ? darkTheme.fontColor : lightTheme.fontColor;
   const cumulativeSum = (sum => value => sum += value)(0);
   const cumulativeData = data.map(raw => {
     return {
@@ -50,19 +62,19 @@ const GraphItem = (props: GraphItemProps) => {
     <Wrapper>
       <Card>
         <Body>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <HeaderDiv>
             <Label>{label}</Label>
             <SwitchDiv>
               <SwitchText>Cummulatief</SwitchText>
               <Toggle id={`cumulativeToggle${keyToggle}`} onOff toggle={toggle} value={isCumulative} />
             </SwitchDiv>
-          </div>
+          </HeaderDiv>
           <div style={{ display: 'flex' }}>
             <ResponsiveContainer width="100%" height={300}>
               <LineChart data={isCumulative ? cumulativeData : data} margin={{ top: 20, right: 20, bottom: 40, left: 20 }}>
-                <Line type="monotone" dataKey="count" stroke="#4E7DD4" dot={{ stroke: lightTheme.hintColor, strokeWidth: 3 }} />
-                <XAxis dataKey="date" angle={25} tickMargin={20} tick={{ stroke: value ? darkTheme.fontColor : lightTheme.fontColor, strokeWidth: 1 }} />
-                <YAxis dataKey="count" tick={{ stroke: value ? darkTheme.fontColor : lightTheme.fontColor, strokeWidth: 1 }} />
+                <Line type="monotone" dataKey="count" stroke="#4E7DD4" dot={{ fill: lightTheme.hintColor, stroke: darkTheme.fontColor, strokeWidth: 0.5 }} />
+                <XAxis dataKey="date" angle={25} tickMargin={20} tick={{ fill: dataColor, stroke: dataColor, strokeWidth: 0.5 }} />
+                <YAxis dataKey="count" tick={{ fill: dataColor, stroke: dataColor, strokeWidth: 0.5 }} />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <Tooltip />
               </LineChart>
