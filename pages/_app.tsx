64c579886fd2dd6777ffdafda36f18ac08a8a700
Jurian Vink <jurianvnk@gmail.com>
2020-05-28 17:53:04 +0200

    sw added, now working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,9 +12,9 @@ const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
     setIsMounted(true);
 
-    // if ('serviceWorker' in navigator) {
-    //   navigator.serviceWorker.register('/service-worker.js');
-    // }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+    }
   }, []);
 
   return (
